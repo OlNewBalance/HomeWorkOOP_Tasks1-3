50a1a04dbd29137c32db25fdcbc0e59d5359548b
Oleg <finkton23@gmail.com>
2025-06-30 23:29:19 +0500

    Update task2WorkWithFeatures.cs

--- HomeWorkOOP/task2WorkWithFeatures.cs
+++ HomeWorkOOP/task2WorkWithFeatures.cs
@@ -1,9 +1,22 @@
-﻿namespace OOP;
-
-internal class task2WorkWithFeatures
+namespace OOP
 {
-    public void Execute()
+    public class Player
     {
-        
+        public double Xcor { get; set; }
+        public double Ycor { get; set; }
+
+        public Player(double x, double y)
+        {
+            this.Xcor = x;
+            this.Ycor = y;
+        }
+    }
+
+    public class Renderer
+    {
+        public static void RenderPlayer(Player player)
+        {
+            Console.WriteLine($"X = {player.Xcor} : Y = {player.Ycor}");
+        }
     }
 }
