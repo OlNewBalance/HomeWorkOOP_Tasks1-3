13e3ba3c2083c335bd6ed116e2e50c7fff50c0b1
Oleg <finkton23@gmail.com>
2025-06-30 23:30:24 +0500

    Update Program.cs

--- HomeWorkOOP/Program.cs
+++ HomeWorkOOP/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace OOP
+{
+internal class Program
+{
+    static public void Main()
+    {
+        Random random = new Random();
+        Human a = new GameCharacter("Harris", random.Next(16, 80), 100, 100, random.Next(1, 100));
+        a.Printinformation();
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Player player = new Player(random.Next(100), random.Next(100));
+        Renderer rend = new Renderer();
+        Renderer.RenderPlayer(player);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        var db = new DataBase();
+        db.AddPlayer("Finkton", 1, 140,false);
+        db.AddPlayer("Waljenuje", 2, 140,false);
+        db.AddPlayer("CanOfPiss", 3, 140,false);
+        db.AddPlayer("Kristina", 4, 140,false);
+        db.BanPlayer(4);
+        db.UnBanPlayer(2);
+        db.DeletePlayer(3);
+        db.PrintPlayers();
+    }
+}
+}
