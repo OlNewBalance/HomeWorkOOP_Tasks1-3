74aca3d4ca304b9afd6c7be375fe61997c328115
Oleg <finkton23@gmail.com>
2025-08-20 22:52:30 +0500

    Update Program.cs

--- HomeWorkOOP/Program.cs
+++ HomeWorkOOP/Program.cs
@@ -1,26 +1,30 @@
 namespace OOP
 {
-internal class Program
-{
-    static public void Main()
+    internal class Program
     {
-        Random random = new Random();
-        Human a = new GameCharacter("Harris", random.Next(16, 80), 100, 100, random.Next(1, 100));
-        a.Printinformation();
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        Player player = new Player(random.Next(100), random.Next(100));
-        Renderer rend = new Renderer();
-        Renderer.RenderPlayer(player);
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        var db = new DataBase();
-        db.AddPlayer("Finkton", 1, 140,false);
-        db.AddPlayer("Waljenuje", 2, 140,false);
-        db.AddPlayer("CanOfPiss", 3, 140,false);
-        db.AddPlayer("Kristina", 4, 140,false);
-        db.BanPlayer(4);
-        db.UnBanPlayer(2);
-        db.DeletePlayer(3);
-        db.PrintPlayers();
+        static public void Main()
+        {
+            Random random = new Random();
+            Player a = new GameCharacter("Harris", random.Next(16, 80), 100, 100, 
+                random.Next(1, 100), random.Next(1, 100), random.Next(1, 2) == 1 ? true : false,
+                random.Next(0, 100));
+            a.PrintInformation();
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Player playerVasiliy = new Player("Vasiliy", 24, 100, 100, 12,
+                random.Next(1, 100), random.Next(1, 2) == 1 ? true : false,
+                random.Next(0, 100));
+            PlayerPosition postition = new PlayerPosition(random.Next(100), random.Next(100));
+            PlayerPosition.PlayerRender();
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            var db = new DataBase();
+            db.AddPlayer("Finkton", 1, 140, false);
+            db.AddPlayer("Waljenuje", 2, 140, false);
+            db.AddPlayer("CanOfPiss", 3, 140, false);
+            db.AddPlayer("Kristina", 4, 140, false);
+            db.BanPlayer(4);
+            db.UnBanPlayer(2);
+            db.DeletePlayer(3);
+            db.PrintPlayers();
+        }
     }
-}
 }
